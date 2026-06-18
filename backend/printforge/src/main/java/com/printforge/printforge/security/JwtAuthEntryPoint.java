d1a34b73ffc05822184c918460a74acd5623e30a
unknown <judefiadzawoo@gmail.com>
2026-06-18 22:22:36 +0000

    authentication is done, yet to add refresh tokens

---
+++ backend/printforge/src/main/java/com/printforge/printforge/security/JwtAuthEntryPoint.java
@@ -0,0 +1,40 @@
+package com.printforge.printforge.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.printforge.printforge.dto.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException {
+
+        ErrorResponse error = ErrorResponse.builder()
+                .status(HttpStatus.FORBIDDEN.value())
+                .message("Access denied")
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+
+        ObjectMapper mapper = new ObjectMapper()
+        .registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule())
+        .disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+mapper.writeValue(response.getWriter(), error);
+    }
+}
